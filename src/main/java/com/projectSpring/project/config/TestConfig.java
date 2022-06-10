019cc4cb3d7321bec48bc269e4d2c7ceec40c1e4
edugabriel12 <gabriellima4539@yahoo.com>
2022-06-10 15:10:05 -0300

    Criando o UserRepository e injetando dados no BD

---
+++ src/main/java/com/projectSpring/project/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.projectSpring.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.projectSpring.project.entities.User;
+import com.projectSpring.project.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public void run(String... args) throws Exception {		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		userRepo.save(u1);
+		userRepo.save(u2);
+	}
+
+}
