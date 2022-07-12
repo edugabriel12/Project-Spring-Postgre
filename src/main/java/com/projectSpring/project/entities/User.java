111f7f7194978455c9d1e539d827702f725c5125
edugabriel12 <gabriellima4539@yahoo.com>
2022-07-11 21:56:07 -0300

    Implementando a classe Order e a relação Order-Client

--- src/main/java/com/projectSpring/project/entities/User.java
+++ src/main/java/com/projectSpring/project/entities/User.java
@@ -1,12 +1,15 @@
 package com.projectSpring.project.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders =  new ArrayList<Order>();
 	
 	public User() {
 	}
@@ -73,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
